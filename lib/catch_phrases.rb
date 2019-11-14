239d24c4a8cd61bad143e4232f9f12cfa5cfb943
Tissiana Alves <altissiana@gmail.com>
2019-11-14 20:29:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,9 @@
 def mario
+<<<<<<< HEAD
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+=======
+  status = "Thank You Mario! But Our Princess Is In Another Castle!\n"
+>>>>>>> bf5a8b61acaac6e3e971d55bd61a6e35da7d2f6b
   phrase = "It's-a me, Mario!\n"
   puts phrase
 end
